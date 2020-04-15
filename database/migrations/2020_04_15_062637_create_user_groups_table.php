a1b0db86189ffb66ac35eb422113fa265780bc26
Pudding <755969423@qq.com>
2020-04-15 14:59:07 +0800

    Create Sql

---
+++ database/migrations/2020_04_15_062637_create_user_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_groups', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->comment('用户组名');
+
+            $table->tinyInteger('level')->defualt(0)->comment('用户组级别')->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_groups');
+    }
+}
---
+++ database/migrations/2020_04_15_065505_create_plugs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlugsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plugs', function (Blueprint $table) {
+
+            $table->id();
+            
+            $table->string('name')->nullable()->comment('轮播标题');
+
+            $table->tinyInteger('active')->default(1)->comment('开启状态');
+
+            $table->tinyInteger('type_id')->comment('所属类型');
+
+            $table->string('images')->nullable()->comment('图片地址');
+
+            $table->tinyInteger('sort')->default(0)->comment('排序权重');
+
+            $table->string('href')->nullable()->default("#")->comment('链接地址');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plugs');
+    }
+}
