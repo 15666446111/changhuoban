d680d46204fe8eb59989e7eb6a18dc18be292172
haha <2896302985>
2020-06-22 17:03:15 +0800

    搜索过滤选项

--- app/Article.php
+++ app/Article.php
@@ -24,25 +24,11 @@ class Article extends Model
     }
 
     /**
-<<<<<<< HEAD
      * [scopeApiGet 搜索过滤选项]
      */
     public function scopeApiGet($query)
     {
     	return $query->where('active', '1')->where('verify',1)->orderBy('sort', 'desc');
     }
-=======
-     * @Author    Pudding
-     * @DateTime  2020-06-22
-     * @copyright [copyright]
-     * @license   [license]
-     * @version   [获取文章缩略图图片地址]
-     * @param     [type]      $value [description]
-     * @return    [type]             [description]
-     */
-    public function getImagesAttribute($value)
-    {
-        return "http://".$_SERVER["HTTP_HOST"]."/".$value;
-    } 
->>>>>>> a459d0e58370c8a4b7180044c87262c0677a214e
+
 }
