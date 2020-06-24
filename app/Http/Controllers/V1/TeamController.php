6aa8ae3b444c1b889875b6e3c306b8609021d611
yuzifei <1213649307@qq.com>
2020-06-24 10:34:49 +0800

    优化下级信息 个人信息

--- app/Http/Controllers/V1/TeamController.php
+++ app/Http/Controllers/V1/TeamController.php
@@ -20,7 +20,7 @@ class TeamController extends Controller
     	try{
             // 获取直接下级信息
             $list = \App\User::/*where('parent', $request->user->id)
-                        ->*/select(['id', 'headimg', 'nickname', 'created_at'])->orderBy('created_at', 'desc')->get();
+                        ->*/select(['id', 'avatar as headimg', 'nickname', 'created_at'])->orderBy('created_at', 'desc')->get();
 
             // 获取总下级人数
             $Arr = \App\UserRelation::where('parents', 'like', "%_".$request->user->id."_%")->pluck('id')->toArray();
@@ -38,7 +38,7 @@ class TeamController extends Controller
 
     	} catch (\Exception $e) {
 
-            return response()->json(['error'=>['message' => '系统错误,联系客服!']]);
+            return response()->json(['error'=>['message' => '系统错误,请联系客服']]);
 
         }
     }
