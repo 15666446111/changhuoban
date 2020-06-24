b56516da7a4a6d375a8020a0a82892e8b524bcbe
haha <2896302985>
2020-06-24 18:20:30 +0800

    12

--- app/Http/Controllers/V1/OrdersController.php
+++ app/Http/Controllers/V1/OrdersController.php
@@ -18,7 +18,6 @@ class OrdersController extends Controller
         $code = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
 
         $order_no = $code[intval(date('Y')) - 2011] . strtoupper(dechex(date('m'))) . date('d') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf('%02d', rand(0, 99));
-        
 
         try{
             $data=\App\Order::create([
