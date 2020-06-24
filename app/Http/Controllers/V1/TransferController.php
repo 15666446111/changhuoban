ae3ac6ff4c7629bddb8e834a57ccf4fc6c6f2580
yuzifei <1213649307@qq.com>
2020-06-24 14:59:31 +0800

    回拨

--- app/Http/Controllers/V1/TransferController.php
+++ app/Http/Controllers/V1/TransferController.php
@@ -112,54 +112,34 @@ class TransferController extends Controller
     {
 
         try{
-                if(!is_array($request->merchant_id)){
-                    $machine = ['0'=>$request->merchant_id];
-                    
-                    \App\Machine::where('user_id',$request->friend_id)
-                    ->where('id',$request->merchant_id)
-                    ->update(['user_id'=>$request->user->id]);
-                    
 
-                    \App\Transfer::where('old_user_id',$request->user->id)
-                    ->where('new_user_id',$request->friend_id)
-                    ->where('machine_id',$request->merchant_id)
-                    ->update(['is_back'=>1]);
+            $res=\App\Machine::whereIn('user_id',$request->friend_id)
+            ->whereIn('id',$request->merchant_id)
+            ->update(['user_id'=>$request->user->id]);
 
-                    foreach($machine as $k=>$v){
-                    
-                        \App\Transfer::create([
-                            'machine_id'    =>  $v,
-                            'old_user_id'   =>  $request->user->id,
-                            'new_user_id'   =>  $request->friend_id,
-                            'state'         =>  $request->state
-                        ]);
-                    }
-                }else{
+            if($res){
 
-                    \App\Machine::where('user_id',$request->friend_id)
-                    ->whereIn('id',$request->merchant_id)
-                    ->update(['user_id'=>$request->user->id]);
-                    
+                $data=\App\MachineLog::where('old_user_id',$request->user->id)
+                ->whereIn('new_user_id',$request->friend_id)
+                ->whereIn('machine_id',$request->merchant_id)
+                ->update(['is_back'=>1]);
 
-                    \App\Transfer::where('old_user_id',$request->user->id)
-                    ->where('new_user_id',$request->friend_id)
-                    ->whereIn('machine_id',$request->merchant_id)
-                    ->update(['is_back'=>1]);
-
-                    foreach($request->merchant_id as $k=>$v){
-                    
-                        \App\Transfer::create([
-                            'machine_id'    =>  $v,
-                            'old_user_id'   =>  $request->user->id,
-                            'new_user_id'   =>  $request->friend_id,
-                            'state'         =>  $request->state
-                        ]);
-                    }
-
+                foreach($request->merchant_id as $k=>$v){
+                    $info = \App\Transfer::create([
+                        'machine_id'    =>  $v,
+                        'old_user_id'   =>  $request->user->id,
+                        'new_user_id'   =>  $request->friend_id,
+                        'state'         =>  $request->state
+                    ]);
                 }
 
-                
-                
+            }
+            
+            if($info){
+
+                return response()->json(['success'=>['message' => '回拨成功!', 'data'=>[]]]);
+
+            }
 
             return response()->json(['success'=>['message' => '回拨成功!', 'data'=>[]]]);
 
