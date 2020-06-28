957f33c46083b5d668143b75daa4b52a9e7c3a05
yuzifei <1213649307@qq.com>
2020-06-28 15:43:51 +0800

    1

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -86,11 +86,6 @@ class ProductController extends AdminController
         $show->field('title', __('产品标题'));
         $show->field('image', __('缩略图'));
         $show->field('active', __('状态'));
-<<<<<<< HEAD
-        // $show->field('type', __('品牌'));
-=======
-        $show->field('style.style_name', __('型号'));
->>>>>>> 0f6c614a5c00a420e9b30f035910c52414a9e594
         $show->field('price', __('价格'));
         $show->field('content', __('内容'));
         $show->field('created_at', __('创建时间'));
@@ -134,15 +129,10 @@ class ProductController extends AdminController
         $form->select('name','类型')->options($type)->load('factory_name','/api/getAdminFactory');
 
         $form->select('factory_name','厂商')->load('style_id','/api/getAdminStyle');
-<<<<<<< HEAD
 
         $form->select('style_id','型号')->required();
 
         $form->ignore(['name','factory_name']);
-=======
-
-        $form->select('style_id','型号');
->>>>>>> 0f6c614a5c00a420e9b30f035910c52414a9e594
 
         $form->currency('price', __('价格'))->symbol('￥');
 
