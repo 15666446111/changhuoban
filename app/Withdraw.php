fbe7e01b7e7c699916600341a26a4fdadc208cfb
yuzifei <1213649307@qq.com>
2020-06-30 18:13:49 +0800

    1

--- app/Withdraw.php
+++ app/Withdraw.php
@@ -9,5 +9,11 @@ class Withdraw extends Model
     
     protected $guarded = [];
 
-
+    /**
+     * 反向关联用户表
+     */
+    public function users()
+    {
+        return $this->belongsTo('App\User','user_id','id');
+    }
 }
