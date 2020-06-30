d8567be0c06d79a0972c882be3d8e32f22942b46
yuzifei <1213649307@qq.com>
2020-06-30 12:38:58 +0800

    修改头像

--- app/Http/Controllers/V1/SetUserController.php
+++ app/Http/Controllers/V1/SetUserController.php
@@ -59,7 +59,7 @@ class SetUserController extends Controller
             $User = \App\User::where('id', $request->user->id)->first(); 
             
             $heading = $request->avatar;
-            var_dump($_FILES);
+            var_dump($heading);
             $filename = uniqid() . '.' . $heading->getClientOriginalExtension();
             
             Storage::disk('file')->put('/'.$filename,file_get_contents($heading->getRealPath()));
