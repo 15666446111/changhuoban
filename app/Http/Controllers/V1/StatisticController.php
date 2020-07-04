4c40583b3326677b6280facc35cf573c8635a98a
yuzifei <1213649307@qq.com>
2020-07-04 11:52:39 +0800

    优化首页统计信息

--- app/Http/Controllers/V1/StatisticController.php
+++ app/Http/Controllers/V1/StatisticController.php
@@ -95,7 +95,7 @@ class StatisticController
         
         $Arr =  $this->getMyTeam();
         
-        return \App\Machine::where('bind_status', '1')->whereBetween('created_at', [ 
+        return \App\Machine::where('bind_status', '1')->whereBetween('bind_time', [ 
                     $this->StartTime,  $this->EndTime])->whereHas('users', function($q) use ($Arr){
                         $q->whereIn('id', $Arr);
                     })->count();     
