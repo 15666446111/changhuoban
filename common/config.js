c685643b7e0ce6c2af169197bec989a095e27bc2
haha <2896302985>
2020-07-04 09:19:22 +0800

    10

--- common/config.js
+++ common/config.js
@@ -1,3 +1,5 @@
-const baseurl = 'http://3.changhuoban.com/api';  //公共接口请求地址
+// const baseurl = 'http://3.changhuoban.com/api';  //公共接口请求地址
+
+const baseurl = 'http://www.changhuoban.com/api';  //公共接口请求地址
 
 export default baseurl;
