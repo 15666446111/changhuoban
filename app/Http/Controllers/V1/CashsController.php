03dd95d15dcfbc51dd968247efc7996a9afbf84b
Pudding <755969423@qq.com>
2020-07-06 18:09:15 +0800

    11

--- app/Http/Controllers/V1/CashsController.php
+++ app/Http/Controllers/V1/CashsController.php
@@ -21,19 +21,19 @@ class CashsController extends Controller
             $type = $request->type ?? 'all';
 
             //总收益
-            $data['revenueAll'] = $request->user->cash->sum('money');
+            $data['revenueAll'] = $request->user->cash->sum('cash_money');
             
             //今日收益
-            $data['revenueDay'] = \App\CashsLog::where('user_id',$request->user->id)->whereDate('created_at', Carbon::today())->sum('money');
+            $data['revenueDay'] = \App\Cashs::where('user_id', $request->user->id)->whereDate('created_at', Carbon::today())->sum('cash_money');
             
             //本月收益
-            $data['revenueMonth'] = \App\CashsLog::where('user_id',$request->user->id)->whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year)->sum('money');
+            $data['revenueMonth'] = \App\Cashs::where('user_id', $request->user->id)->whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year)->sum('cash_money');
 
             // 查询用户账号余额
             $data['balance'] = $request->user->wallets->cash_blance + $request->user->wallets->return_blance;
             
             // 
-            $list = \App\CashsLog::where('user_id', $request->user->id);
+            $list = \App\Cashs::where('user_id', $request->user->id);
             // 收益类型
             if($type == 'cash'){
                 $list->whereIn('type', ['1', '2']);
@@ -43,16 +43,18 @@ class CashsController extends Controller
                 $list->whereIn('type', ['3','4','5', '6', '7', '8']);
             }
 
+
             if($type == 'other'){
-                $list->whereIn('type', ['9']);
+                $list->whereIn('type', ['10']);
             }        
             
             $list = $list->groupBy('date')->orderBy('date', 'desc')->get(
                         array(
                             DB::raw('Date(created_at) as date'),
-                            DB::raw('SUM(money) as money')
+                            DB::raw('SUM(cash_money) as money')
                         )
                     );
+
             $weekarray=array("日","一","二","三","四","五","六");
             
             foreach ($list as $key => $value) {
@@ -60,7 +62,7 @@ class CashsController extends Controller
                 $dt = Carbon::parse($value->date);
                 
                 //dd(\App\Cash::where('user_id', $request->user->id)->whereDay('created_at', $value->date)->orderBy('created_at', 'desc')->get());
-                $listdata = \App\CashsLog::where('user_id', $request->user->id)->whereDate('created_at', $value->date);
+                $listdata = \App\Cashs::where('user_id', $request->user->id)->whereDate('created_at', $value->date);
 
                 if($type == 'cash'){
                     $listdata->whereIn('type', ['1', '2']);
@@ -71,7 +73,7 @@ class CashsController extends Controller
                 }
     
                 if($type == 'other'){
-                    $listdata->whereIn('type', ['9']);
+                    $listdata->whereIn('type', ['10']);
                 }        
                 
                 
